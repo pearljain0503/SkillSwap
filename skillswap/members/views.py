3ee58988db2a9ba40aaa34df8c62cad90543d018
Tanish Jain <tanishj52@gmail.com>
2026-01-30 01:47:32 +0530

    Completed Django Setup

--- skillswap/members/views.py
+++ skillswap/members/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
+def members(request) :
+    return HttpResponse("Hello world")
+    # template = loader.get_template('myfirst.html')
+    # return HttpResponse(template.render())
 # Create your views here.
